7ddacac506ab9599c1260d5667f3387f5abdcdbc
Leanherediadev <lea.200048@gmail.com>
2025-06-25 19:47:59 -0300

    Codigo de la resta (#2)

---
+++ Resta.py
@@ -0,0 +1,11 @@
+# Calculadora de resta en Python
+
+# Pedimos al usuario que ingrese dos números
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+
+# Realizamos la resta
+resultado = num1 - num2
+
+# Mostramos el resultado
+print("El resultado de la resta es:", resultado)
